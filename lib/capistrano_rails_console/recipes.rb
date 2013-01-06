f1635144b5247cfff675a7c21afaf104ca544f10
Timo Schilling <timo@schilling.io>
2013-01-06 01:59:43 +0100

    support for rails 2

--- lib/capistrano_rails_console/recipes.rb
+++ lib/capistrano_rails_console/recipes.rb
@@ -5,7 +5,14 @@ Capistrano::Configuration.instance(:must_exist).load do
     desc "Open a rails console the first app server"
     task :console, :roles => :app do
       hostname = find_servers_for_task(current_task).first
-      exec "ssh -l #{user} #{hostname} -p #{port} -t 'cd #{current_path} && rails c #{rails_env}'"
+
+      console_command = case defined?(Rails::VERSION::MAJOR) && Rails::VERSION::MAJOR
+      when 2
+        "script/rails console #{rails_env}"
+      else
+        "rails console #{rails_env}"
+      end
+      exec "ssh -l #{user} #{hostname} -p #{port} -t 'cd #{current_path} && #{console_command}'"
     end
   end
 end
