82abf8420539da972fc6990709853ce19fd353cb
Timo Schilling <timo@schilling.io>
2013-01-08 04:48:12 +0100

    refactor recipes for specs

--- lib/capistrano_rails_console/recipes.rb
+++ lib/capistrano_rails_console/recipes.rb
@@ -1,14 +1,24 @@
-require "capistrano/configuration"
+require "capistrano"
 require "capistrano_rails_console/rails"
 
-Capistrano::Configuration.instance(:must_exist).load do
-  namespace :rails do
-    desc "Open a rails console the first app server"
-    task :console, :roles => :app do
-      hostname = find_servers_for_task(current_task).first
+module CapistranoRailsConsole
+  module Recipes
+    def self.load_into(configuration)
+      configuration.load do
+        namespace :rails do
+          desc "Open a rails console the first app server"
+          task :console, :roles => :app do
+            hostname = find_servers_for_task(current_task).first
 
-      console_command = CapistranoRailsConsole::Rails.console_command % rails_env
-      exec "ssh -l #{user} #{hostname} -p #{port} -t 'cd #{current_path} && #{console_command}'"
+            console_command = CapistranoRailsConsole::Rails.console_command % rails_env
+            exec "ssh -l #{user} #{hostname} -p #{port} -t 'cd #{current_path} && #{console_command}'"
+          end
+        end
+      end
     end
   end
+end
+
+if Capistrano::Configuration.instance
+  CapistranoRailsConsole.load_into(Capistrano::Configuration.instance)
 end
