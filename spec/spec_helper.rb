52396ae848d69082bad1f6d69131735ab02e11ec
Timo Schilling <timo@schilling.io>
2013-01-08 05:38:36 +0100

    filter specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,4 +6,6 @@ require "capistrano_rails_console/recipes"
 RSpec.configure do |config|
   config.include Capistrano::Spec::Matchers
   config.include Capistrano::Spec::Helpers
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
 end
